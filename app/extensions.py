31038804e11883f0c263c4033cfcd62457d94398
fahdsalim19977-sys <fahdsalim19977@gmail.com>
2026-08-19 15:04:07 +0300

    Add files via upload

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,4 @@
 # app/extensions.py
-from flask_wtf.csrf import CSRFProtect
-
-# ===== إضافات Flask =====
-csrf = CSRFProtect()
-
 def init_extensions(app):
     """تهيئة جميع الإضافات"""
-    csrf.init_app(app)
+    pass
